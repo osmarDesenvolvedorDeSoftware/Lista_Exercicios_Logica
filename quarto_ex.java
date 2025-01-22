05c4bef50288aa9818bf9cd0bfff4925e32bb16d
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-22 16:45:09 -0300

    Add files via upload

---
+++ quarto_ex.java
@@ -0,0 +1,25 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+//1 - Faça um algoritmo valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class Main {
+  public static void main(String[] args) {
+
+  int numero=3; // altere aqui para testar outros valores
+
+    if(numero%2==0){
+      System.out.println("O número é par");
+    }else{
+      System.out.println("O número é impar");
+    }
+
+    
+
+    
+  }
+
+}
+
+ 
