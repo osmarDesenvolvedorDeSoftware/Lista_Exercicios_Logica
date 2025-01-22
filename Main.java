fc5b055672a2a275522537a1a9fe54f3c1a34c51
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-22 00:32:05 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+//1 - Faça um algoritmo valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class Main {
+  public static void main(String[] args) {
+     int a= 5; int b = 1; int c = 20; int soma =0;
+     soma = a + b;
+     System.out.println("A soma de A e B é: "+ soma);
+
+    if(soma < c){
+      System.out.println("A soma de A e B é menor que C");
+    }
+    
+  }
+
+ 
+}
