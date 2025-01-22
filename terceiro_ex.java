d6cbb131e5c1b87f159f0ebd811069959ea2dd76
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-22 16:41:33 -0300

    Add files via upload

---
+++ terceiro_ex.java
@@ -0,0 +1,30 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+//1 - Faça um algoritmo valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class Main {
+  public static void main(String[] args) {
+
+  int numero1=10; int numero2=5; // altere aqui para testar os numeros
+
+      if(numero1 > numero2){
+
+        System.out.println("O numero 1 é maior que o numero 2");
+      }else if(numero2 > numero1){
+
+        System.out.println("O numero 2 é maior que o numero 1");
+        
+      }else{
+
+        System.out.println("Os numeros são iguais");
+      }
+    
+    
+      }
+
+    
+  }
+
+ 
