2dcede34e3be6d24e7836d1439064624def64383
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-22 16:54:50 -0300

    Add files via upload

---
+++ quinto_ex.java
@@ -0,0 +1,28 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+//1 - Faça um algoritmo valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class Main {
+  public static void main(String[] args) {
+
+  double nota1, nota2, nota3, media;
+    // altere as notas para os valores que desejar
+    nota1= 10;
+    nota2= 10;
+    nota3 = 7;
+    media = (nota1+nota2+nota3) /3;
+
+      if (media > 7){
+        System.out.println("Aprovado");
+      }else{
+          System.out.println("Reprovado");
+      }
+
+    
+  }
+
+}
+
+ 
