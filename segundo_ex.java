211e42f19fc8946e3056d93000c92a035658698d
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-22 16:36:32 -0300

    Add files via upload

---
+++ segundo_ex.java
@@ -0,0 +1,32 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+//1 - Faça um algoritmo valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class Main {
+  public static void main(String[] args) {
+
+    int numero = 0; // aqui altere para fazer os testes
+
+      if (numero > 0){
+
+        System.out.println("o numero é positivo");
+                
+      }
+
+      if(numero < 0){
+
+        System.out.println("o numero é negativo");
+      }
+
+      if(numero == 0){
+
+        System.out.println("o numero é zero");
+      }
+
+    
+  }
+
+ 
+}
