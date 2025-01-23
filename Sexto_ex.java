620fe42fa7aec0fe83ccc21306c5a28417d3d0ec
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-23 10:54:35 -0300

    Add files via upload

---
+++ Sexto_ex.java
@@ -0,0 +1,25 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int idade = 10;
+
+      if(idade < 12){
+        System.out.println("Criança");
+      }else if(idade >= 13 && idade <17){
+        System.out.println("Adolescente");        
+      }else if (idade >= 18 && idade <64){
+        System.out.println("Adulto");
+      }else System.out.println("idoso");
+      
+
+    
+  }
+
+}
+
+ 
