d17ec52c4bad01624bef595d71e9e9032c30fa84
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-23 11:26:37 -0300

    Add files via upload

---
+++ oitavo_ex.java
@@ -0,0 +1,23 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+   int a =8; int b = 10; int c = 10;
+
+    if(a > b && a > c){
+      System.out.println("o A é maior");
+    }else if(b > a && b > c){
+      System.out.println("o B é maior");
+    }else if (c > a && c > b){
+      System.out.println("o C é maior");
+    }else System.out.println("Os numero são iguais");
+    
+  }
+
+}
+
+ 
