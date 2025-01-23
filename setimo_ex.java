f15a7bd914c615f4640710658b00fc7a57baa3e7
Osmar F Cavalcante <osmar@osmardev.online>
2025-01-23 11:19:38 -0300

    Add files via upload

---
+++ setimo_ex.java
@@ -0,0 +1,19 @@
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+   int taxa = 5; double km_percorrido = 3.5;
+    int valor_km = 2; double valorTotal = taxa + (km_percorrido * valor_km);
+
+    System.out.println("O valor total a ser pago é: " + valorTotal);
+
+    
+  }
+
+}
+
+ 
